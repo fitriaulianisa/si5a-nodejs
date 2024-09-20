cb4b0ca709aa2f7149eb798374baac912573b5ac
TWCS911 <aulianisafitri434@gmail.com>
2024-09-20 11:02:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,17 @@
 const { log } = require('console');
 const http = require('http');
 
-//buat server
+// buat server
 const server = http.createServer( (req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/ html' });
-    res.write('Selamat Pagi');
+    // res.writeHead(200, { 'Content-Type': 'text/ html' });
+    // res.write('Selamat Pagi');
+    
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.write(JSON.stringify({
+        'message' : 'selamat pagi',
+        'status' : 'success'
+    }));
     res.end();
 
 });
