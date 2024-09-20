0f9e9e4d514295fd38dc2057d73ae2d0637a53f9
TWCS911 <aulianisafitri434@gmail.com>
2024-09-20 11:11:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,23 @@ const server = http.createServer( (req, res) => {
     
 
     res.writeHead(200, { 'Content-Type': 'application/json' });
+
+    if(req.url === '/dosen'){
+        message = 'List Data Dosen';
+        data = ['Ahmad', 'Hafiz', 'Widi'];
+    } else if(req.url ==='/mahasiswa'){
+        message = 'List Data Mahasiswa';
+        data = ['Kenny', 'Rizky', 'Akbar'];
+
+    } else {
+        message = 'Tidak Ditemukan';
+        data = [];
+
+
+    }
     res.write(JSON.stringify({
-        'message' : 'selamat pagi',
+        'message' : message,
+        'data' : data,
         'status' : 'success'
     }));
     res.end();
